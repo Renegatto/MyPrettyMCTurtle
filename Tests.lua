e05ce4da8de58af56b304a5384c6d3fede9fa715
Renegatto <46404781+Renegatto@users.noreply.github.com>
2020-06-11 08:11:33 +0300

    Create Tests.lua

---
+++ Tests.lua
@@ -0,0 +1,86 @@
+require "Calculations"
+
+function show(s,x) print(s..tostring(x)) return x end
+function showt(s,t) 
+  for k, v in pairs(t) do
+    print(s..tostring(v)) 
+  end
+  return t 
+end
+function showa(s,t)
+  print(string.format("%s (n %s,e %s,s %s ,w %s)",s,t[0],t[1],t[2],t[3]))
+  return t
+end
+showa("n",Direction.from_angle(0))
+showa("e",Direction.from_angle(1))
+showa("s",Direction.from_angle(2))
+showa("w",Direction.from_angle(3))
+function ang_conversions()
+   local ang = math.random(0,3)
+   
+   assert(
+    Direction.as_angle(Direction.from_angle(ang)) == ang,
+    "as_angle or from_angle failed test"
+   )
+   
+end
+function const(x) return function(ignored) return x end end
+function rotate_test()
+  local rotates_for_identity = {
+    {rotates = 4,   ang = Direction.from_angle(1)},
+    {rotates = 2,   ang = Direction.from_angle(2)},
+    {rotates = 4,   ang = Direction.from_angle(3)},
+    {rotates = 1,   ang = Direction.from_angle(0)},
+  }
+  
+  local rand = function() return Direction.from_angle(math.random(0,3)) end
+  
+  local rnd = rand()
+  
+  local rotates_results = table.map( -- [[(int -> int)]]
+    function(identity)
+      return table.fold(  -- (Direction -> (Direction -> Direction) -> Direction) 
+                          -- -> [(Direction -> Direction)]
+                          -- -> Direction
+                          -- -> Direction
+        function(acc,fn) return fn(acc) end,
+        
+        table.map( -- (a -> (Direction -> Direction)) -> [int] -> [(Direction -> Direction)]
+        
+          const(function(acc) -- (a -> Direction -> Direction)
+            return Direction.rotate(
+                acc, 
+                identity.ang
+              )
+          end),
+          table.range(1,identity.rotates)
+        ),
+        rnd
+    )
+    end, 
+    rotates_for_identity
+  )
+  table.map(
+    function(x) assert(x == Direction.as_angle(rnd), "test failed "..tostring(x).."; "..tostring(Direction.as_angle(rnd)) ) end,
+    table.map(
+      function(x) return Direction.as_angle(x) end,
+      rotates_results
+    )
+  )
+end
+
+--print(
+--  table.fold(
+--    function(acc,fn1)return fn1(acc) end,
+--    table.map(
+--      const(function(acc)return acc+acc end), 
+--      table.range(1,100)
+--    ),
+--    1
+--   )
+--)
+for x=0,1000 do
+   ang_conversions()
+   rotate_test()
+end
+
